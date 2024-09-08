73e616f410c01c3d11ef18cf01fde4cf5889a3d4
Austin Milt <austin.w.milt@gmail.com>
2024-09-07 20:10:55 -0400

    remove unused env variables

--- lib/env.dart
+++ lib/env.dart
@@ -5,26 +5,14 @@ class Env {
   // use the `const` keyword in order for them to be loaded before being
   // processed to their final values during class initialization. Consequently
   // we have to do some weird stuff so that we can use `const`.
-  static final String gcpServiceAccountKey = _base64String(
-    _K.gcpServiceAccountKey,
-    const bool.hasEnvironment(_K.gcpServiceAccountKey) ? const String.fromEnvironment(_K.gcpServiceAccountKey) : null,
-  );
-
   static final String loggingImplementation = _string(
     _K.loggingImplementation,
     const bool.hasEnvironment(_K.loggingImplementation) ? const String.fromEnvironment(_K.loggingImplementation) : null,
   );
-
-  static final String mainDbSheetId = _string(
-    _K.mainDbSheetId,
-    const bool.hasEnvironment(_K.mainDbSheetId) ? const String.fromEnvironment(_K.mainDbSheetId) : null,
-  );
 }
 
 class _K {
-  static const String gcpServiceAccountKey = 'GCP_SERVICE_ACCOUNT_KEY_BASE64';
   static const String loggingImplementation = 'LOGGING_IMPLEMENTATION';
-  static const String mainDbSheetId = 'MAIN_DB_SHEET_ID';
 }
 
 Duration _duration(String name, int? duration, TimeUnit unit) {
